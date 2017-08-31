71fc1c1e39d1bca93e969580e2aa4bf951b598ce
krw252 <30392048+krw252@users.noreply.github.com>
2017-08-31 10:15:06 -0400

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,22 @@
 # Hint:  use Google to find python function
 
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+from datetime import datetime
+date_start = '01-02-2013'    
+date_stop = '07-28-2015'
+days_between = datetime.strptime(date_stop, '%m-%d-%Y') - datetime.strptime(date_start, '%m-%d-%Y')
+print(days_between)
 
 ####b)  
+from datetime import datetime
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015' 
+days_between = datetime.strptime(date_stop, '%m%d%Y') - datetime.strptime(date_start, '%m%d%Y')
+print(days_between)
 
 ####c)  
-date_start = '15-Jan-1994'  
+from datetime import datetime
+date_start = '15-Jan-1994'      
 date_stop = '14-Jul-2015'  
+days_between = datetime.strptime(date_stop, '%d-%b-%Y') - datetime.strptime(date_start, '%d-%b-%Y')
+print(days_between)
