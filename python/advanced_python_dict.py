721d395092ca6ee019f97c03157b812dfc1972ca
krw252 <30392048+krw252@users.noreply.github.com>
2017-09-11 19:08:08 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,5 +1,6 @@
 
 import csv
+from collections import OrderedDict
 
 file = '/users/krw252/ds/metis/metisgh/prework/dsp/python/faculty.csv'
 
@@ -14,7 +15,7 @@ def read_csv():
 
 faculty = read_csv()
 
-def get_dict():
+def faculty_dict():
     del faculty[0]
     names = []
     for row in faculty:
@@ -28,4 +29,20 @@ def get_dict():
     final = dict(zip(names,info))
     return (final)
 
-print (get_dict())
+print(list(faculty_dict().items())[:3])
+
+def professor_dict():
+    del faculty[0]
+    names = []
+    for row in faculty:
+        name = row[0]
+        last_name = (name.rsplit()[0], name.rsplit()[-1])
+        names.append(last_name)
+    info = []
+    for row in faculty:
+        del row[0]
+        info.append(row)
+    final = OrderedDict(zip(names,info))
+    return (final)
+
+print(list(professor_dict().items())[:3])
