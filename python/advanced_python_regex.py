327975737870910a128d6fabec8919743bde03af
krw252 <30392048+krw252@users.noreply.github.com>
2017-09-11 15:32:46 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,51 +1,11 @@
 import csv
 import re
 
-facultycsv = """name, degree, title, email
-Scarlett L. Bellamy, Sc.D.,Associate Professor of Biostatistics,bellamys@mail.med.upenn.edu
-Warren B. Bilker,Ph.D.,Professor of Biostatistics,warren@upenn.edu
-Matthew W Bryan, PhD,Assistant Professor of Biostatistics,bryanma@upenn.edu
-Jinbo Chen, Ph.D.,Associate Professor of Biostatistics,jinboche@upenn.edu
-Susan S Ellenberg, Ph.D.,Professor of Biostatistics,sellenbe@upenn.edu
-Jonas H. Ellenberg, Ph.D.,Professor of Biostatistics,jellenbe@mail.med.upenn.edu
-Rui Feng, Ph.D,Assistant Professor of Biostatistics,ruifeng@upenn.edu
-Benjamin C. French, PhD,Associate Professor of Biostatistics,bcfrench@mail.med.upenn.edu
-Phyllis A. Gimotty, Ph.D,Professor of Biostatistics,pgimotty@upenn.edu
-Wensheng Guo, Ph.D,Professor of Biostatistics,wguo@mail.med.upenn.edu
-Yenchih Hsu, Ph.D.,Assistant Professor of Biostatistics,hsu9@mail.med.upenn.edu
-Rebecca A Hubbard, PhD,Associate Professor of Biostatistics,rhubb@mail.med.upenn.edu
-Wei-Ting Hwang, Ph.D.,Associate Professor of Biostatistics,whwang@mail.med.upenn.edu
-Marshall M. Joffe, MD MPH Ph.D,Professor of Biostatistics,mjoffe@mail.med.upenn.edu
-J. Richard Landis, B.S.Ed. M.S. Ph.D.,Professor of Biostatistics,jrlandis@mail.med.upenn.edu
-Yimei Li, Ph.D.,Assistant Professor of Biostatistics,liy3@email.chop.edu
-Mingyao Li, Ph.D.,Associate Professor of Biostatistics,mingyao@mail.med.upenn.edu
-Hongzhe Li, Ph.D,Professor of Biostatistics,hongzhe@upenn.edu
-A. Russell Localio, JD MA MPH MS PhD,Associate Professor of Biostatistics,rlocalio@upenn.edu
-Nandita Mitra, Ph.D.,Associate Professor of Biostatistics,nanditam@mail.med.upenn.edu
-Knashawn H. Morales, Sc.D.,Associate Professor of Biostatistics,knashawn@mail.med.upenn.edu
-Kathleen Joy Propert, Sc.D.,Professor of Biostatistics,propert@mail.med.upenn.edu
-Mary E. Putt, PhD ScD,Professor of Biostatistics,mputt@mail.med.upenn.edu
-Sarah Jane Ratcliffe, Ph.D.,Associate Professor of Biostatistics,sratclif@upenn.edu
-Michelle Elana Ross, PhD,Assistant Professor is Biostatistics,michross@upenn.edu
-Jason A. Roy, Ph.D.,Associate Professor of Biostatistics,jaroy@mail.med.upenn.edu
-Mary D. Sammel, Sc.D.,Professor of Biostatistics,msammel@cceb.med.upenn.edu
-Pamela Ann Shaw, PhD,Assistant Professor of Biostatistics,shawp@upenn.edu
-Russell Takeshi Shinohara,0,Assistant Professor of Biostatistics,rshi@mail.med.upenn.edu
-Haochang Shou, Ph.D.,Assistant Professor of Biostatistics,hshou@mail.med.upenn.edu
-Justine Shults, Ph.D.,Professor of Biostatistics,jshults@mail.med.upenn.edu
-Alisa Jane Stephens, Ph.D.,Assistant Professor of Biostatistics,alisaste@mail.med.upenn.edu
-Andrea Beth Troxel, ScD,Professor of Biostatistics,atroxel@mail.med.upenn.edu
-Rui Xiao, PhD,Assistant Professor of Biostatistics,rxiao@mail.med.upenn.edu
-Sharon Xiangwen Xie, Ph.D.,Associate Professor of Biostatistics,sxie@mail.med.upenn.edu
-Dawei Xie, PhD,Assistant Professor of Biostatistics,dxie@upenn.edu
-Wei (Peter) Yang, Ph.D.,Assistant Professor of Biostatistics,weiyang@mail.med.upenn.edu"""
-
-with open('faculty.csv', 'w') as f:
-    f.write(facultycsv)
+file = '/users/krw252/ds/metis/metisgh/prework/dsp/python/faculty.csv'
 
 def read_csv():
     faculty_data = []
-    data = open('faculty.csv','r')
+    data = open(file ,'r')
     reader = csv.DictReader(data)
     for row in reader:
         faculty_data.append(row)
@@ -95,4 +55,51 @@ def count_degrees(csv_file_name):
             degree_dict['JD'] = degree_dict['JD'] + 1
     return (degree_dict)
 
+def count_titles(csv_file_name):
+    titles = []
+    for row in faculty:
+        titles.append(row[' title'])
+    titles_dict = dict.fromkeys(titles,0)
+    for row in faculty:
+        if re.match('Associate' ,str(row[' title'])):
+            titles_dict['Associate Professor of Biostatistics'] = titles_dict['Associate Professor of Biostatistics'] + 1
+    for row in faculty:
+        if re.match('Professor' ,str(row[' title'])):
+            titles_dict['Professor of Biostatistics'] = titles_dict['Professor of Biostatistics'] + 1
+    for row in faculty:
+        if re.match('Assistant' ,str(row[' title'])):
+            titles_dict['Assistant Professor of Biostatistics'] = titles_dict['Assistant Professor of Biostatistics'] + 1
+    return (titles_dict)
+
+def emails(data):
+    emails = []
+    for row in data:
+        emails.append(row[' email'])
+    return (emails)
+
+def domains(email_list):
+    domains = []
+    for e in email_list:
+        d = e.split('@')
+        domains.append(d[1])
+    return (set(domains))
+
+def get_dict():
+    del faculty[0]
+    names = []
+    for row in faculty:
+        name = row[0]
+        last_name = name.rsplit()[0]
+        names.append(last_name)
+    info = []
+    for row in faculty:
+        del row[0]
+        info.append(row)
+    final = dict(zip(names,info))
+    return (final)
+
 print(count_degrees(faculty))
+print(count_titles(faculty))
+print(emails(faculty))
+print (domains(emails(faculty)))
+print (get_dict())
